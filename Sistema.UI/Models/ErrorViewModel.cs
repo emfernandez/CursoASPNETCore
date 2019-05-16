c0dda8dfb409d0c798329b462f418b96f178ba8b
Emmanuel Fernandez <Emmanuel.Fernandez@cablasociados.com>
2019-05-16 10:44:59 -0300

    Commit inicial

---
+++ Sistema.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
